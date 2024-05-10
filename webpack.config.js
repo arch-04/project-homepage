de7d0f6ce119c139e08e37836ecd4cb20e464c23
Ivan_s <sjtepanovicivan34@gmail.com>
2024-05-10 22:56:52 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath:'/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
